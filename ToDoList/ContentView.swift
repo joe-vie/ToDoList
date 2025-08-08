3aa146b72696f16db587e5cad98d1a00cf21063e
Jovie <marcemg101@gmail.com>
2025-08-08 13:08:54 -0400

    Set up the ToDoList title and plus button :3

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -10,11 +10,22 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            HStack {
+                Text("To-Do List")
+                    .font(.title)
+                    .font(.system(size:40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color(red: 0.4549019607843137, green: 0.5529411764705883, blue: 0.6823529411764706))
+                Spacer() //push left/right
+                Button {
+                    
+                } label: {
+                    Text("+")
+                        .foregroundColor(Color(red: 0.4549019607843137, green: 0.5529411764705883, blue: 0.6823529411764706))
+                }
+            } // end of HStack
+            Spacer() //push up/down
+        } // end of the Vstack
         .padding()
     }
 }
